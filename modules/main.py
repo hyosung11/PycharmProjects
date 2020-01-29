25e3621123d125a2fb0aba11a908857d360d0440
HyoSung Bidol-Lee <hyosung11@me.com>
2020-01-28 21:06:48 -0500

    first commit to github

---
+++ modules/main.py
@@ -0,0 +1,9 @@
+# One method
+from utility import multiply, divide
+
+# Another method - can avoid naming conflict e.g., 'max
+from shopping.more_shopping import shopping_cart
+
+print(shopping_cart.buy('apple'))
+print(divide(5, 2))
+print(multiply(5, 2))
