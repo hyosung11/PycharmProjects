e872514018c3479c2d21a6488ce0ff44429d1a38
HyoSung Bidol-Lee <hyosung11@me.com>
2020-01-29 12:14:29 -0500

    built-in modules

--- modules/main.py
+++ modules/main.py
@@ -1,10 +1,34 @@
 # One method
-from utility import multiply, divide
+# from utility import multiply, divide
 
 # Another method - can avoid naming conflict e.g., 'max
-from shopping.more_shopping import shopping_cart
+# from shopping.more_shopping import shopping_cart
 
-print(shopping_cart.buy('apple'))
-print(divide(5, 2))
-print(multiply(5, 2))
+# if __name__ == '__main__':
+#     print(shopping_cart.buy('apple'))
+#     print(divide(5, 2))
+#     print(multiply(5, 2))
+#
+#     print('please run this')
 
+# RANDOM Built-in Module
+
+# import random
+
+# alternative import convention
+# from random import shuffle
+# import random as awesome # to avoid name collision
+
+# import only what you need
+# from random import shuffle
+#
+# my_list = [4,8,16,32,64]
+# shuffle(my_list)
+# print(my_list)
+
+
+# SYS Built-in Module
+
+import sys
+
+print(sys)
